14552ee6f9bcf17f1d44697d8c7606cc0a0e7105
rodrigoguzman1 <rjguzman@miuandes.cl>
2020-06-08 13:34:24 -0400

    Cambios Perfil

--- SonicWFA/Perfil.cs
+++ SonicWFA/Perfil.cs
@@ -12,14 +12,71 @@ namespace SonicWFA
 {
     public partial class Perfil : Form
     {
-        public Perfil()
+        Usuario2 usuarioActual;
+        public Perfil(Usuario2 usuario)
         {
             InitializeComponent();
+
+            this.usuarioActual = usuario;
+            labelUsuario.Text = usuario.nombreDeUsuario;
+            labelNombre.Text = usuario.nombre;
+            labelApellido.Text = usuario.apellido;
+            if (usuario.tipoUsuario == "SI")
+            {
+                labelTipo.Text = "PREMIUM";
+            } else
+            {
+                labelTipo.Text = "GRATIS";
+            }
+            labelPrivacidad.Text = usuario.privacidad;
+        }
+
+        private void Actualizar()
+        {
+
+            labelUsuario.Text = usuarioActual.nombreDeUsuario;
+            labelNombre.Text = usuarioActual.nombre;
+            labelApellido.Text = usuarioActual.apellido;
+            if (usuarioActual.tipoUsuario == "SI")
+            {
+                labelTipo.Text = "PREMIUM";
+            }
+            else
+            {
+                labelTipo.Text = "GRATIS";
+            }
+            labelPrivacidad.Text = usuarioActual.privacidad;
         }
 
         private void btnFlecha_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void labelUsuario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            usuarioActual.CambiarInfo(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, comboBox1.Text, textBox5.Text);
+            Actualizar();
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            comboBox1.Text = "";
         }
     }
 }

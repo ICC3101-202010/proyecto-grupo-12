0bdbe7151dfd1867f8baeec955e48f8f12c09a9a
Javier Soto <jisoto2@miuandes.cl>
2020-06-08 18:33:01 -0400

    Admin

---
+++ respaldo Entrega 3/entrega3/entrega3/funcionesAdmin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace entrega3
+{
+    public partial class funcionesAdmin : Form
+    {
+        public funcionesAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form1 ins = new Form1();
+            ins.MdiParent = this.MdiParent;
+            this.Hide();
+            ins.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            agregarAdmin ins = new agregarAdmin();
+            ins.MdiParent = this.MdiParent;
+            this.Hide();
+            ins.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            agregarPublicidad ins = new agregarPublicidad();
+            ins.MdiParent = this.MdiParent;
+            this.Hide();
+            ins.ShowDialog();
+        }
+    }
+}
